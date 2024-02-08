6adae6a44107f06f3af74ec8c70d5f00c5b1a3b6
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2024-02-08 20:05:01 -0300

    WIP: Read TXT Function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
-import psutil
+import psutil as ps
 
 def map_TXT_Driver(arquivo):
     with open("DriverMapped.txt", "a") as file:
         file.writelines(arquivo + '\n')
+
+
 ...
+
 
 def listar_drivers():
     try:
-        Current_Devices = psutil.disk_partitions(all=True)
+        Current_Devices = ps.disk_partitions(all=True)
 
         if Current_Devices:
             print("Lista de Drivers:")
@@ -19,19 +22,35 @@ def listar_drivers():
 
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
+
+
 ...
 
 
-def ProcurarArquivo():
+def procurarArquivo():
+    start = "C:\\"
 
-        start = "C:\\"
+    for dirpath, dirnames, filenames in os.walk(start):
+        for filename in filenames:
+            filename = os.path.join(dirpath, filename)
+            map_TXT_Driver(filename)
 
-        for dirpath, dirnames, filenames in os.walk(start):
-            for filename in filenames:
-
-                    filename = os.path.join(dirpath, filename)
-                    map_TXT_Driver(filename)
 
 ...
 
-ProcurarArquivo()
+# ***** WORK IN PROGRESS! *****
+def ler_txt():
+    palavras = []
+
+    with open("DriverMapped.txt", "r") as rd_file:
+
+     ler =  rd_file.read()
+    print(ler)
+    # O .read() não retorna todas as linhas presentes no arquivo txt.
+    # Readlines() também não retornou todas as linhas.
+    #Arquivo .txt está dando por volta de 74mb com mais de 10 mil linhas
+    #Encontrar uma forma de talvez dividir em chunks,
+    ...
+
+procurarArquivo()
+
