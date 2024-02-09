ba39f5168bb532824eb7d314adc51e436015d48e
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2024-02-09 18:26:26 -0300

    feat: read .txt , added TODO.MD and still finishing the classes

---
+++ TODO.md
@@ -0,0 +1,13 @@
+## Find your Path Project:
+
+***
+
+#### Funcionalidades:
+
+- Mapear Disco rígido em um arquivo .txt
+- Listar discos rígidos presentes na máquina e perguntar ao usuário qual ele gostaria de "varrer"
+
+- Perguntar ao usuário o que ele gostaria de procurar em específico em sua máquina.
+- Ler o arquivo .txt e caso exista o que ele procura retornar a ele.
+
+___
--- main.py
+++ main.py
@@ -1,56 +1,38 @@
 import os
 import psutil as ps
 
-def map_TXT_Driver(arquivo):
+def map_txt_Driver(arquivo):
     with open("DriverMapped.txt", "a") as file:
         file.writelines(arquivo + '\n')
-
-
 ...
 
 
-def listar_drivers():
-    try:
-        Current_Devices = ps.disk_partitions(all=True)
+def varrer_e_mapear():
 
-        if Current_Devices:
-            print("Lista de Drivers:")
-            for dispositivo in Current_Devices:
-                print(f"Nome: {dispositivo.device}, Tipo: {dispositivo.fstype}")
-        else:
-            print("Nenhum driver encontrado.")
-
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
-
-
-...
-
-
-def procurarArquivo():
     start = "C:\\"
 
     for dirpath, dirnames, filenames in os.walk(start):
         for filename in filenames:
             filename = os.path.join(dirpath, filename)
-            map_TXT_Driver(filename)
-
-
+            map_txt_Driver(filename)
 ...
 
 # ***** WORK IN PROGRESS! *****
 def ler_txt():
-    palavras = []
 
     with open("DriverMapped.txt", "r") as rd_file:
 
-     ler =  rd_file.read()
-    print(ler)
-    # O .read() não retorna todas as linhas presentes no arquivo txt.
-    # Readlines() também não retornou todas as linhas.
-    #Arquivo .txt está dando por volta de 74mb com mais de 10 mil linhas
-    #Encontrar uma forma de talvez dividir em chunks,
+        for linha, conteudo in enumerate(rd_file):
+
+            if "Curriculo" in conteudo:
+
+                print("Linha: " , linha)
+                print("Caminho: " , conteudo)
+
+        else:
+            print("________________")
+            print("Fim da busca.")
+
     ...
 
-procurarArquivo()
-
+ler_txt()
