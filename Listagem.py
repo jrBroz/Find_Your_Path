b0165fa8ecf68899b172716240837e332b9c74a0
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2024-02-11 15:13:00 -0300

    feat: pedir para usuario digitar qual driver varrer , falta deixar codigo mais limpo

---
+++ Listagem.py
@@ -0,0 +1,23 @@
+import psutil as ps
+
+def listar_drivers():
+    try:
+        dispositivo_atual = ps.disk_partitions(all=True)
+
+        if dispositivo_atual:
+            print("Lista de Drivers:")
+
+        for dispositivo in dispositivo_atual:
+            print(f"Nome: {dispositivo.device}, Tipo: {dispositivo.fstype}")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+        ...
+
+
+
+    def escolher_driver():
+        pass
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import os
 import psutil as ps
+import Listagem
+import time
+
+
 
 def map_txt_Driver(arquivo):
-    with open("DriverMapped.txt", "a") as file:
+    with open("DriverMapped.txt", "a" , encoding="utf-8") as file:
         file.writelines(arquivo + '\n')
 ...
 
 
 def varrer_e_mapear():
+    start2 = time.perf_counter()
+    Listagem.listar_drivers()
 
-    start = "C:\\"
+    start = input("Qual Driver voce deseja varrer: ")
 
     for dirpath, dirnames, filenames in os.walk(start):
         for filename in filenames:
             filename = os.path.join(dirpath, filename)
             map_txt_Driver(filename)
+
+            end = time.perf_counter()
+            duracao = round(end - start2)
+
+    print('duracao em segundos: ' , duracao)
+
 ...
 
-# ***** WORK IN PROGRESS! *****
+
+
 def ler_txt():
 
     with open("DriverMapped.txt", "r") as rd_file:
@@ -35,4 +48,6 @@ def ler_txt():
 
     ...
 
-ler_txt()
+varrer_e_mapear()
+
+
