2775923a5e534a18d57991d33d9c454cd53271eb
Broz <158494991+jrBroz@users.noreply.github.com>
2024-03-12 15:02:34 -0300

    Main contém apenas a chamada da gui com funcionalidades

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import threading
-import time
-from modules import ler
-from modules import varrer as varrer_driver
+from modules import GUI
 # --------------------------------------
 
-def iniciar_programa():
-
-    thread_varrer = threading.Thread(target=varrer_driver.varrer_e_mapear)
-    thread_ler = threading.Thread(target=ler.ler_txt)
-
-    thread_varrer.start()
-    thread_varrer.join()
-    thread_ler.start()
-    ...
-iniciar_programa()
+GUI.iniciar_programa()
